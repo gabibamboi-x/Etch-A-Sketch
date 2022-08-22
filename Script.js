5744813e7f9386aef7052fc93f6e3e04999fabea
Gabriel Bamboi <gabi.bamboi_x@icloud.com>
2022-08-22 20:18:40 +0200

    Add new div to be loaded before the sketchbook

--- Script.js
+++ Script.js
@@ -1,27 +1,43 @@
-const container = document.getElementById('mainDiv');
 
-let q = 3
+// create a 'background' div, this way the title, the sketchbook
+// and the buttons will all be in a box
+const backdiv = document.getElementById('backgroundDiv');
 
-for (let i = 0; i < q; i++) {
-    let y = document.createElement('div');
-    y.setAttribute('class', 'parentDiv')
-    container.appendChild(y)
+const container = document.createElement('div');
+const title = document.createElement('h1');
+
+container.setAttribute('id', 'mainDiv');
+title.setAttribute('id', 'title');
+title.textContent = 'Etch A Sketch';
+
+backdiv.appendChild(title)
+backdiv.appendChild(container);
+
+
+
+let grid = 10;
+// create a row of divs
+for (let i = 0; i < grid; i++) {
+let y = document.createElement('div');
+y.setAttribute('class', 'parentDiv');
+container.appendChild(y);   
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    for (let j = 0; j < q; j++) {
-        const row = document.querySelectorAll('.parentDiv')
-        row.forEach(el => {
-            let x = document.createElement('div');
-            x.setAttribute('id', 'column')
-            el.appendChild(x)})
-}});
+// add the same amount of divs to every item in the row,
+// with css, change their direction and get a grid
+// with the same number of rows and columns
+for (let j = 0; j < grid; j++) {
+    const row = document.querySelectorAll('.parentDiv');
+    row.forEach(el => {
+        let x = document.createElement('div');
+        x.setAttribute('id', 'column');
+        el.appendChild(x);
+})};
 
-window.addEventListener('DOMContentLoaded', () => {
-    const sq = document.querySelectorAll('#column')
 
+// listen for mouseover event on each square and change their color
+window.addEventListener('DOMContentLoaded', () => {
+    const sq = document.querySelectorAll('#column');
     sq.forEach(el => el.addEventListener('mouseover', () => {
-        el.style.backgroundColor = 'black'}))
-})
-
-
+        el.style.backgroundColor = 'black';
+}))});

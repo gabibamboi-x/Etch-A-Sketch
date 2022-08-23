b413666658717b09f832ea6913252cab0796b02c
Gabriel Bamboi <gabi.bamboi_x@icloud.com>
2022-08-23 21:55:39 +0200

    Implement the option to create a custom grid

--- Script.js
+++ Script.js
@@ -1,43 +1,60 @@
 
-// create a 'background' div, this way the title, the sketchbook
+// create a 'background' div, this way the sketchbook
 // and the buttons will all be in a box
 const backdiv = document.getElementById('backgroundDiv');
-
 const container = document.createElement('div');
-const title = document.createElement('h1');
+const buttonsDiv = document.createElement('div');
 
 container.setAttribute('id', 'mainDiv');
-title.setAttribute('id', 'title');
-title.textContent = 'Etch A Sketch';
+buttonsDiv.setAttribute('id', 'buttonsDiv');
 
-backdiv.appendChild(title)
 backdiv.appendChild(container);
+backdiv.appendChild(buttonsDiv);
+
+// initial grid creation
+let grid = 20;
+createGrid();
 
 
-
-let grid = 10;
-// create a row of divs
-for (let i = 0; i < grid; i++) {
-let y = document.createElement('div');
-y.setAttribute('class', 'parentDiv');
-container.appendChild(y);   
+// adding the buttons
+for (let i = 0; i < 5; i++) {
+    let j = document.createElement('button');
+    j.textContent = 'button' + i;
+    j.setAttribute('id', ('button' + i));
+    buttonsDiv.appendChild(j);
 }
 
-// add the same amount of divs to every item in the row,
-// with css, change their direction and get a grid
-// with the same number of rows and columns
-for (let j = 0; j < grid; j++) {
-    const row = document.querySelectorAll('.parentDiv');
-    row.forEach(el => {
-        let x = document.createElement('div');
-        x.setAttribute('id', 'column');
-        el.appendChild(x);
-})};
+const gridLayout = document.getElementById('button0');
+
+gridLayout.addEventListener('click', () => {
+    grid = prompt('Layout: ');
+    if (grid > 100) {
+        grid = 100;
+    }
+    container.innerHTML = ''
+    createGrid();
+});
 
 
-// listen for mouseover event on each square and change their color
-window.addEventListener('DOMContentLoaded', () => {
-    const sq = document.querySelectorAll('#column');
-    sq.forEach(el => el.addEventListener('mouseover', () => {
-        el.style.backgroundColor = 'black';
-}))});
+function createGrid() {
+    // create a row of divs
+    for (let i = 0; i < grid; i++) {
+        let y = document.createElement('div');
+        y.setAttribute('class', 'parentDiv');
+        container.appendChild(y);   
+        }
+        
+        // add the same amount of divs to every item in the row,
+        // with css, change their direction and get a grid
+        // with the same number of rows and columns
+        for (let j = 0; j < grid; j++) {
+            const row = document.querySelectorAll('.parentDiv');
+            row.forEach(el => {
+                let x = document.createElement('div');
+                x.setAttribute('id', 'column');
+                // add event listener for each square
+                x.addEventListener('mouseover', () => {
+                    x.style.backgroundColor = 'black';
+                })
+                el.appendChild(x);
+})}};
